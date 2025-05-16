b152e04dae52aeb3a3ffae69b9555551f3ae6112
diyaaistwal <151556055+diyaaistwal@users.noreply.github.com>
2025-05-16 19:34:41 +0530

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,44 @@
+import ply.yacc as yacc
+from lexer import tokens
+
+# Grammar rules
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = ('+', p[1], p[3])
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = ('-', p[1], p[3])
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = ('*', p[1], p[3])
+
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    p[0] = ('/', p[1], p[3])
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    p[0] = p[1]
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+def p_error(p):
+    if p:
+        print(f"Syntax error at '{p.value}'")
+    else:
+        print("Syntax error at EOF")
+
+parser = yacc.yacc()
